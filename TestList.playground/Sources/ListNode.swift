5fbddb11060fab876da15ad4c6ea2325a52c9092
beforeold <beforeold@163.com>
2022-08-16 17:52:02 +0800

    add reverseLinkedlist2

--- TestList.playground/Sources/ListNode.swift
+++ TestList.playground/Sources/ListNode.swift
@@ -24,4 +24,26 @@ public class Solution {
         
         return newHead
     }
+    
+    public static func reversList2(_ head: ListNode?) -> ListNode? {
+        if head == nil { return head }
+        if head?.next == nil { return head }
+        
+        var head = head
+        var newHead: ListNode?
+        
+        while (head != nil) {
+            // save next head
+            let nextHead = head?.next
+            
+            // change new head
+            head?.next = newHead
+            newHead = head
+            
+            // change head
+            head = nextHead
+        }
+        
+        return newHead
+    }
 }
