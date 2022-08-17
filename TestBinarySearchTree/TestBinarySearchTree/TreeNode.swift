b0e370ebd23d708d9ca17660e7cb2f60b172bfed
beforeold <beforeold@163.com>
2022-08-17 18:52:43 +0800

    add BinarySearchTree

---
+++ TestBinarySearchTree/TestBinarySearchTree/TreeNode.swift
@@ -0,0 +1,85 @@
+//
+//  TreeNode.swift
+//  TestBinarySearchTree
+//
+//  Created by Brook16 on 2022/8/17.
+//
+
+import Foundation
+
+
+public class TreeNode: CustomDebugStringConvertible {
+    public typealias Element = Int
+    
+    public var left: TreeNode?
+    public var right: TreeNode?
+    
+    /// 注意：这里必须使用弱指针，否则调用clear的时候，root置位nil，其他节点不会销毁
+    public weak var parent: TreeNode?
+    
+    public var height: Int = 1
+    
+    public var element: Element
+    
+    public var val: Element {
+        get {
+            return self.element
+        }
+        set {
+            self.element = newValue
+        }
+    }
+    
+    public var isLeaf: Bool {
+        return (left == nil && right == nil)
+    }
+    
+    public var hasTowChildren: Bool {
+        return (left != nil && right != nil)
+    }
+    
+    public init() {
+        self.element = 0;
+        self.left = nil;
+        self.right = nil;
+    }
+    
+    public init(_ val: Int) {
+        self.element = val;
+        self.left = nil;
+        self.right = nil;
+    }
+    
+    public init(_ val: Int,
+                _ left: TreeNode?,
+                _ right: TreeNode?)
+    {
+        self.element = val
+        self.left = left
+        self.right = right
+    }
+    
+    public init(element: Element, parent: TreeNode?) {
+        self.element = element
+        self.parent = parent
+    }
+    
+    deinit {
+       
+    }
+    
+    public var debugDescription: String {
+        if let parentElement = parent?.element {
+            return "\(element)(\(parentElement))"
+        }else {
+            return "\(element)"
+        }
+    }
+}
+
+extension TreeNode: Equatable {
+    public static func == (lhs: TreeNode, rhs: TreeNode) -> Bool {
+        return Unmanaged.passRetained(lhs).toOpaque() == Unmanaged.passRetained(rhs).toOpaque()
+        // return lhs.element == rhs.element
+    }
+}
---
+++ TestBinarySearchTree/TestBinarySearchTree/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  TestBinarySearchTree
+//
+//  Created by Brook16 on 2022/8/17.
+//
+
+import Foundation
+
+
+let tree = BinarySearchTree()
+
+print(tree)
