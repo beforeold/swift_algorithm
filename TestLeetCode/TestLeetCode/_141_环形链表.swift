dc676764237c4c71a9ce5c6bf174d8ae7b04ce32
beforeold <ibeforeold@gmail.com>
2022-09-02 15:27:09 +0800

    add hasCycle

--- TestLeetCode/TestLeetCode/_141_环形链表.swift
+++ TestLeetCode/TestLeetCode/_141_环形链表.swift
@@ -7,35 +7,27 @@
 
 import Foundation
 
-
-
 /// https://leetcode.cn/problems/linked-list-cycle/
 class Solution141 {
-  
-  public class ListNode {
-      public var val: Int
-      public var next: ListNode?
-      public init(_ val: Int) {
-          self.val = val
-          self.next = nil
-      }
-  }
-  
-  /**
-   * Definition for singly-linked list.
-   * public class ListNode {
-   *     public var val: Int
-   *     public var next: ListNode?
-   *     public init(_ val: Int) {
-   *         self.val = val
-   *         self.next = nil
-   *     }
-   * }
-   */
-
   class Solution {
-      func hasCycle(_ head: ListNode?) -> Bool {
-          false
+    func hasCycle(_ head: ListNode?) -> Bool {
+      guard head?.next != nil else {
+        return false
       }
+      
+      var slow = head
+      var fast = head?.next
+      while fast != nil && fast?.next != nil {
+        
+        if fast === slow {
+          return true
+        }
+        
+        slow = slow?.next
+        fast = fast?.next?.next
+      }
+      
+      return false
+    }
   }
 }
