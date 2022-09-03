91474b343ae364f6d8fe54ce0c638a66f9796299
beforeold <ibeforeold@gmail.com>
2022-09-03 20:22:49 +0800

    add preorderTraversal with stack

--- TestLeetCode/TestLeetCode/binary_tree/_144_二叉树的前序遍历.swift
+++ TestLeetCode/TestLeetCode/binary_tree/_144_二叉树的前序遍历.swift
@@ -10,7 +10,14 @@ import Foundation
 // https://leetcode.cn/problems/binary-tree-preorder-traversal/
 
 class Solution144 {
+  /// 递归实现
   class S1 {
+    static func test() {
+      let root = TreeNode.create(from: [1, 2, 3, 4, 5, 6, 7])
+      let ret = Solution().preorderTraversal(root)
+      assert(ret == [1, 2, 4, 5, 3, 6, 7])
+    }
+    
     class Solution {
       func preorderTraversal(_ root: TreeNode?) -> [Int] {
         guard let root = root else {
@@ -28,18 +35,37 @@ class Solution144 {
 }
 
 extension Solution144 {
+  /// 使用 stack 进行迭代实现
   class S2 {
+    static func test() {
+      let root = TreeNode.create(from: [1, 2, 3, 4, 5, 6, 7])
+      let ret = Solution().preorderTraversal(root)
+      assert(ret == [1, 2, 4, 5, 3, 6, 7])
+    }
+    
     class Solution {
       func preorderTraversal(_ root: TreeNode?) -> [Int] {
-        guard let root = root else {
-          return []
+        guard let root = root else { return [] }
+        
+        var result = [Int]()
+        
+        var stack = [TreeNode]()
+        stack.append(root)
+        
+        while !stack.isEmpty {
+          let popped = stack.removeLast()
+          result.append(popped.val)
+          
+          if let right = popped.right {
+            stack.append(right)
+          }
+          
+          if let left = popped.left {
+            stack.append(left)
+          }
         }
         
-        var array = [root.val]
-        array.append(contentsOf: preorderTraversal(root.left))
-        array.append(contentsOf: preorderTraversal(root.right))
-        
-        return array
+        return result
       }
     }
   }
--- TestLeetCode/TestLeetCode/main.swift
+++ TestLeetCode/TestLeetCode/main.swift
@@ -34,6 +34,9 @@ Solution234.S3.test()
 Solution83.S1.test()
 Solution83.S2.test()
 
+Solution144.S1.test()
+Solution144.S2.test()
+
 Solution102.S1.test()
 Solution102.S2.test()
 
