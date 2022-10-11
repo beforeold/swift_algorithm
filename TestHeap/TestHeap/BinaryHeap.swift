2501f5300464941620336030d11862665385c57b
beforeold <ibeforeold@gmail.com>
2022-10-11 16:48:18 +0800

    add remove() for heap

--- TestHeap/TestHeap/BinaryHeap.swift
+++ TestHeap/TestHeap/BinaryHeap.swift
@@ -8,128 +8,178 @@
 import Foundation
 
 public protocol Heap {
-    associatedtype Element: Comparable
-    
-    /// get size of the heap
-    ///
-    /// - Complexity: O(1)
-    var size: Int { get }
-    
-    /// depend on the size
-    ///
-    /// - Complexity: O(1)
-    var isEmpty: Bool { get }
-    
-    /// clear the heap
-    func clear()
-    
-    /// add an element to keep the heap feature
-    func add(_ element: Element)
-    
-    /// get the root element
-    func get() -> Element
-    
-    /// remove the root element
-    func remove() -> Element
-    
-    /// replace the root element
-    func replace(_ element: Element) -> Element
+  associatedtype Element: Comparable
+  
+  /// get size of the heap
+  ///
+  /// - Complexity: O(1)
+  var size: Int { get }
+  
+  /// depend on the size
+  ///
+  /// - Complexity: O(1)
+  var isEmpty: Bool { get }
+  
+  /// clear the heap
+  func clear()
+  
+  /// add an element to keep the heap feature
+  func add(_ element: Element)
+  
+  /// get the root element
+  func get() -> Element
+  
+  /// remove the root element
+  func remove() -> Element
+  
+  /// replace the root element
+  func replace(_ element: Element) -> Element
 }
 
 
 public func HeapDefaultComparator<Element: Comparable>(_ e1: Element, _ e2: Element) -> Int {
-    if e1 < e2 {
-        return -1
-    }
-    else if e1 > e2 {
-        return 1
-    }
-    return 0
+  if e1 < e2 {
+    return -1
+  }
+  else if e1 > e2 {
+    return 1
+  }
+  return 0
 }
 
 public class BinaryHeap<Element: Comparable>: Heap {
-    public private(set) var size: Int = 0
-    private var container = [Element?]()
-    private let comparator: (_ e1: Element, _ e2: Element) -> Int
-    
-    public init(comparator: @escaping (_ e1: Element, _ e2: Element) -> Int = HeapDefaultComparator) {
-        self.comparator = comparator
+  public private(set) var size: Int = 0
+  private var container = [Element?]()
+  private let comparator: (_ e1: Element, _ e2: Element) -> Int
+  
+  public init(comparator: @escaping (_ e1: Element, _ e2: Element) -> Int = HeapDefaultComparator) {
+    self.comparator = comparator
+  }
+  
+  public var isEmpty: Bool {
+    return size == 0
+  }
+  
+  public func clear() {
+    for i in 0..<size {
+      container[i] = nil
     }
     
-    public var isEmpty: Bool {
-        return size == 0
-    }
+    size = 0
+  }
+  
+  public func add(_ element: Element) {
+    container.append(element)
+    size += 1
     
-    public func clear() {
-        for i in 0..<size {
-            container[i] = nil
+    siftUp(size - 1)
+  }
+  
+  public func get() -> Element {
+    emptyCheck()
+    
+    return container[0]!
+  }
+  
+  public func remove() -> Element {
+    emptyCheck()
+    
+    let rootValue = element(at: 0)
+    let tail = container[size - 1]
+    container[0] = tail
+    container[size - 1] = nil
+    size -= 1
+    
+    siftUp(0)
+    
+    return rootValue
+  }
+  
+  public func replace(_ element: Element) -> Element {
+    fatalError()
+  }
+  
+  // MARK: - private
+  private func siftUp(_ index: Int) {
+    var index = index
+    let iValue = element(at: index)
+    
+    while index > 0 {
+      let parentIndex = (index - 1) / 2
+      let parentValue = element(at: parentIndex)
+      
+      if comparator(parentValue, iValue) >= 0 {
+        return
+      }
+      
+      // swap
+      container[parentIndex] = iValue
+      container[index] = parentValue
+      
+      index = parentIndex
+    }
+  }
+  
+  private func siftDown(_ index: Int) {
+    let iValue = element(at: index)
+    var index = index
+    
+    // 完全二叉树性质：计算最小非叶子结点的个数
+    let half = size / 2
+    
+    while index < half {
+      // 由于右子结点可能不存在，所以先默认赋值为左子结点
+      var childIndex = 2 * index + 1
+      var childValue = element(at: childIndex)
+      
+      // 判断右子树是否可能为更大值
+      let rightIndex = childIndex + 1
+      if rightIndex < size {
+        let rightValue = element(at: rightIndex)
+        if comparator(rightValue, childValue) > 0 {
+          childIndex = rightIndex
+          childValue = rightValue
         }
-        
-        size = 0
+      }
+      
+      if comparator(iValue, childValue) > 0 {
+        break
+      }
+      
+      container[index] = childValue
+      index = childIndex
     }
     
-    public func add(_ element: Element) {
-        container.append(element)
-        size += 1
-        
-        siftUp(size - 1)
+    container[index] = iValue
+  }
+  
+  private func siftUp_opt(_ index: Int) {
+    var index = index
+    let iValue = element(at: index)
+    
+    while index > 0 {
+      let parentIndex = (index - 1) / 2
+      let parentValue = element(at: parentIndex)
+      
+      if comparator(parentValue, iValue) >= 0 {
+        break
+      }
+      
+      // pass down parent value and continue
+      container[index] = parentValue
+      index = parentIndex
     }
     
-    public func get() -> Element {
-        return container[0]!
+    container[index] = iValue
+  }
+  
+  private func element(at index: Int) -> Element {
+    return container[index]!
+  }
+  
+  private func emptyCheck() {
+    if isEmpty {
+      fatalError("the heap is empty")
     }
-    
-    public func remove() -> Element {
-        fatalError()
-    }
-    
-    public  func replace(_ element: Element) -> Element {
-        fatalError()
-    }
-    
-    // MARK: - private
-    private func siftUp(_ index: Int) {
-        var index = index
-        let iValue = element(at: index)
-        
-        while index > 0 {
-            let pIndex = (index - 1) / 2
-            let pValue = element(at: pIndex)
-            
-            if comparator(pValue, iValue) >= 0 {
-                return
-            }
-            
-            // swap
-            container[pIndex] = iValue
-            container[index] = pValue
-            
-            index = pIndex
-        }
-    }
-    
-    private func siftUp_opt(_ index: Int) {
-        var index = index
-        let iValue = element(at: index)
-        
-        while index > 0 {
-            let pIndex = (index - 1) / 2
-            let pValue = element(at: pIndex)
-            
-            if comparator(pValue, iValue) >= 0 {
-                break
-            }
-            
-            // pass down parent value and continue
-            container[index] = pValue
-            
-            index = pIndex
-        }
-        
-        container[index] = iValue
-    }
-    
-    private func element(at index: Int) -> Element {
-        return container[index]!
-    }
+  }
 }
